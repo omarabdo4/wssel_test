6fd74042600236856106a83d9888e665e2e2e41e
Omar Abdo <omar.abdelfatah44@gmail.com>
2020-11-19 08:41:12 +0200

    migrations

---
+++ database/migrations/2020_11_18_234725_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('time');
+            $table->float('amount', 8, 2);
+            $table->foreignId('shoptype_id');
+            $table->foreignId('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
