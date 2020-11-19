87512f94c9112e8c6a40f0d4fbb2215090435b0f
Omar Abdo <omar.abdelfatah44@gmail.com>
2020-11-19 12:10:58 +0200

    shoptype seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(ShoptypeSeeder::class);
+        // $this->call(CustomerSeeder::class);
+        // $this->call(OrderSeeder::class);
     }
 }
---
+++ database/seeders/ShoptypeSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Shoptype;
+
+class ShoptypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $inputFileName = './database/seeders/Database.xlsx';
+        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+        $reader->setReadDataOnly(true);
+        $sheet = $reader->load($inputFileName)->setActiveSheetIndexByName('order');
+
+        $typesArray = [];
+
+        foreach ($sheet->getRowIterator() as $rowkey => $row) {
+            if($rowkey == 1){
+                continue;
+            }
+            array_push($typesArray,$sheet->getCell('F'.$row->getRowIndex())->getValue());
+
+            // dump( 'row key ' . $rowkey);
+            // dump( 'row index ' . $row->getRowIndex());
+            // dump( 'shop type ' . );
+            // $cellIterator = $row->getCellIterator() ;
+            // $cellIterator->setIterateOnlyExistingCells(FALSE);
+            // foreach ( $cellIterator as $cellkey => $cell) {
+            //     dump($cell->getValue());
+            // }
+        }
+        $typesArrayDistinct = array_unique($typesArray);
+        
+        Shoptype::truncate();
+
+        foreach ($typesArrayDistinct as $type) {
+            $shoptype = new Shoptype;
+            $shoptype->type = $type;
+            $shoptype->save();
+        }
+
+
+    }
+}
